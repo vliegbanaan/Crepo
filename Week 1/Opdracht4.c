551096659f839311f7bcdaf81f2760b5b596cf5a
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2021-11-30 18:44:52 +0100

    fixed added
    veel toegevoegd, krijg alleen niet werkend dat de andere main vanuit opdracht 1 wordt aangeroepen.. moet ik een extra functie toevoegen aan opdracht 1 en die aanroepen in opdracht 3?

--- Week 1/Opdracht4.c
+++ Week 1/Opdracht4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void omtrek(float *x, float *y) //berekent omtrek van cirkel met Pointer arg.
 {
-    *y=(22 *( *x )) / (7); // resultaat van bereking wordt in y opgeslagen.
+    *y=(22 *( *x )) / (7); // resultaat van bereking wordt in y opgeslagen, breuk voor PI.
 }
 
 int main()
 {
     float z = 1,x;
-    printf("Vul de diameter in: ");
+    printf("Vul de diameter in: ");             // vraag gebruiker voor invoer.
     scanf("%f", &x);
         omtrek(&x, &z);
     printf("Omtrek = %f\n", z);
